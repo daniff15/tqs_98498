d9c76d94d6ec3a36dc3ade4ac27f491d88930340
daniff15 <dani.fig@ua.pt>
2022-04-01 23:19:05 +0100

    4c

---
+++ lab3/lab3_2/demo/src/test/java/com/example/demo/CarRepoTest.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class CarRepoTest {
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Test
+    public void whenFindByModel_thenCar() {
+
+        Car car = new Car("Nissan", "GT-R");
+
+        testEntityManager.persistAndFlush(car);
+
+        Car response_car = carRepository.findByCarID(car.getCarID());
+
+        assertThat(response_car.getCarID()).isEqualTo(car.getCarID());
+    }
+
+    @Test
+    public void whenInvalidModel_thenNoCar() {
+
+        Car car = carRepository.findByCarID(-500L);
+
+        assertThat(car).isNull();
+    }
+}
