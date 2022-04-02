5f8af8ae9edee7ba2bddca51f850974a0162e6ea
daniff15 <dani.fig@ua.pt>
2022-04-02 18:45:09 +0100

    integration test

--- lab3/lab3_2/demo/src/test/java/com/example/demo/CarControllerTemplateTest.java
+++ lab3/lab3_2/demo/src/test/java/com/example/demo/CarControllerTemplateTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,16 +13,20 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import java.util.List;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.demo.Car;
 import com.example.demo.CarRepository;
+import com.example.demo.DemoApplication;
 
 import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-
-@AutoConfigureTestDatabase
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = DemoApplication.class)
+@AutoConfigureMockMvc
+// @AutoConfigureTestDatabase
+@TestPropertySource(locations = "application-integrationtest.properties")
 public class CarControllerTemplateTest {
     // will need to use the server port for the invocation url
     @LocalServerPort
@@ -53,8 +58,10 @@ public class CarControllerTemplateTest {
         createTestCar("Nissan", "GT-R");
         createTestCar("Bugatti", "Centodieci");
 
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromHttpUrl("http://localhost:" + randomServerPort + "/api/cars");
         ResponseEntity<List<Car>> response = restTemplate
-                .exchange("/api/cars", HttpMethod.GET, null, new ParameterizedTypeReference<List<Car>>() {
+                .exchange(builder.toUriString(), HttpMethod.GET, null, new ParameterizedTypeReference<List<Car>>() {
                 });
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
