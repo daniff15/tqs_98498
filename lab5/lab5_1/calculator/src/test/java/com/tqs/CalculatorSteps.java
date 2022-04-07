34754ee7ab649c122120b4b6179a8d4fc5b2e778
daniff15 <dani.fig@ua.pt>
2022-04-07 17:14:50 +0100

    calculator test done

---
+++ lab5/lab5_1/calculator/src/test/java/com/tqs/CalculatorSteps.java
@@ -0,0 +1,39 @@
+package com.tqs;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CalculatorSteps {
+
+    private Calculator calc;
+
+    @Given("a calculator I just turned on")
+    public void a_calculator_i_just_turned_on() {
+        calc = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void i_add_and(int arg1, int arg2) {
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("+");
+    }
+
+    @When("I substract {int} to {int}")
+    public void substract(int arg1, int arg2) {
+        calc.push(arg1);
+        calc.push(arg2);
+        calc.push("-");
+    }
+
+    @Then("the result is {int}")
+    public void the_result_is(double expected) {
+        Number value = calc.value();
+        assertEquals(expected, value);
+    }
+
+}
